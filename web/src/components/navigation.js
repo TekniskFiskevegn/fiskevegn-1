fe533c533b246ccd16f5ce9123a625643fb332b2
Håvard Holen <havardholen@Havards-MacBook-Pro.local>
2020-07-22 11:39:20 +0200

    new release

--- web/src/components/navigation.js
+++ web/src/components/navigation.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import {Link} from 'gatsby'
-import PropTypes from 'prop-types'
-import {cn} from '../lib/helpers'
-import {TiArrowRight} from 'react-icons/ti'
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import { cn } from "../lib/helpers";
+import { TiArrowRight } from "react-icons/ti";
 
-import styles from './navigation.module.css'
-import stylesItem from './navigation-item.module.css'
-import utils from './utils.module.css'
+import styles from "./navigation.module.css";
+import stylesItem from "./navigation-item.module.css";
+import utils from "./utils.module.css";
 
-const Navigation = ({nodes, template, locale}) => {
+// comment
+
+const Navigation = ({ nodes, template, locale }) => {
   return (
     <div className={styles.root}>
       <ul className={cn(styles.default, styles.custom, styles.boxShadow)}>
         {nodes &&
           nodes.map(node => {
-            const {name, navigationText, slug, alternativeSlug} = node.basicTemplate
+            const { name, navigationText, slug, alternativeSlug } = node.basicTemplate;
             const linkTo = () => {
               if (alternativeSlug) {
-                if (locale === 'en') {
-                  return `/${alternativeSlug}`
+                if (locale === "en") {
+                  return `/${alternativeSlug}`;
                 } else {
-                  return `/no/${alternativeSlug}`
+                  return `/no/${alternativeSlug}`;
                 }
               } else {
-                return `/${template[locale]}/${slug.current}`
+                return `/${template[locale]}/${slug.current}`;
               }
-            }
+            };
             return (
               <li key={node.id}>
                 <Link className={stylesItem.root} to={linkTo()}>
@@ -48,15 +50,15 @@ const Navigation = ({nodes, template, locale}) => {
                   </div>
                 </Link>
               </li>
-            )
+            );
           })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navigation.propTypes = {
   nodes: PropTypes.array.isRequired
-}
+};
 
-export default Navigation
+export default Navigation;
