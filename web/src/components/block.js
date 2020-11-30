88cd461cf0f1b594a348f6e5dba4a303cd01053a
Håvard Holen <havardholen@Havards-MacBook-Pro.local>
2020-11-30 14:45:43 +0100

    news update

--- web/src/components/block.js
+++ web/src/components/block.js
@@ -3,7 +3,6 @@ import { cn } from "../lib/helpers";
 import VerticalRhythm from "../components/vertical-rhythm";
 
 import styles from "./block.module.css";
-//comment
 
 const Block = ({ name, ...props }) => {
   const { borderBottom } = props;
